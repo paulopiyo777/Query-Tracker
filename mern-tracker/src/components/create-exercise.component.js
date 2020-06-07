a08330943ebe11ca02df8e1ca8b73a8e9a10a33e
Paul Opiyo <paulopiyo777@gmail.com>
2020-06-07 01:44:32 -0500

    modify frontend

--- mern-tracker/src/components/create-exercise.component.js
+++ mern-tracker/src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -22,10 +23,15 @@ export default class CreateExercises extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user',
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e) {
@@ -63,6 +69,11 @@ export default class CreateExercises extends Component {
         }
 
         console.log(exercise);
+
+         // make http request to backend server
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
+
 
         window.location = '/';
     }
--- mern-tracker/src/components/create-user.component.js
+++ mern-tracker/src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUsers extends Component {
     constructor(props) {
@@ -26,6 +27,10 @@ export default class CreateUsers extends Component {
         }
 
         console.log(user);
+
+        // make http request to backend server
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
 
         this.setState({
             username: ''
